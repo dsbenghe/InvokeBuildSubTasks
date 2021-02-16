f3641b8cbc7eedfb21eb267397186c3d8e727092
Roman Kuzmin <nightroman@gmail.com>
2021-02-16 13:37:09 +0000

    support 2+ root tasks...
    - completion of mixed tasks works
    - completion of parameters does not (why?), just shows root parameters, but it works on calling
    	- caveat: a parameter should be common for all sub scripts

--- README.md
+++ README.md
@@ -32,3 +32,28 @@
     BuildParam1
     BuildParam2
 
+
+This works:
+
+    root.build.ps1 build, deploy task1 -CommonChildParam zzz
+
+with output
+
+```
+Build build, deploy C:\-\InvokeBuildSubTasks\root.build.ps1
+Task /build
+Build task1 C:\-\InvokeBuildSubTasks\src\build.build.ps1
+Task /task1
+child 1 task 1  zzz
+Done /task1 00:00:00.0070356
+Build succeeded. 1 tasks, 0 errors, 0 warnings 00:00:00.0560018
+Done /build 00:00:00.0789647
+Task /deploy
+Build task1 C:\-\InvokeBuildSubTasks\deploy\deploy.build.ps1
+Task /task1
+child 2 task 1  zzz
+Done /task1 00:00:00.0060003
+Build succeeded. 1 tasks, 0 errors, 0 warnings 00:00:00.0189746
+Done /deploy 00:00:00.0259660
+Build succeeded. 4 tasks, 0 errors, 0 warnings 00:00:00.2759678
+```
--- deploy/deploy.build.ps1
+++ deploy/deploy.build.ps1
@@ -1,12 +1,14 @@
 param(
     [Parameter(Position=0)]
     [string[]]$Tasks,
- 
+
 	[string]$DeployParam1,
-	
-	[string]$DeployParam2
+
+	[string]$DeployParam2,
+
+	[string]$CommonChildParam
 )
- 
+
 Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
 
@@ -24,14 +26,14 @@ if ([System.IO.Path]::GetFileName($MyInvocation.ScriptName) -ne 'Invoke-Build.ps
 	Invoke-Build -Task $Tasks -File $MyInvocation.MyCommand.Path @PSBoundParameters
 	return
 }
- 
+
 task task1 {
-    Write-Output "child 2 task 1 $DeployParam1"
+    Write-Output "child 2 task 1 $DeployParam1 $CommonChildParam"
 }
- 
+
 task deploy-task2 {
-    Write-Output "child 2 task 2 $DeployParam2"
+    Write-Output "child 2 task 2 $DeployParam2 $CommonChildParam"
 }
- 
+
 # Synopsis: Default task.
 task . task1, deploy-task2
