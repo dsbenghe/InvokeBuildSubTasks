0abddb329e37f5b4212062114c651a7dd9f8d8f3
Roman Kuzmin <nightroman@gmail.com>
2021-02-16 06:36:10 +0000

    add completer for SubTasks

--- root.build.ps1
+++ root.build.ps1
@@ -1,23 +1,34 @@
 param(
     [Parameter(Position=0)]
     [string[]]$Tasks,
- 
+
     [Parameter(Position=1)]
+	[ArgumentCompleter({
+		param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
+		$task = $fakeBoundParameters['Tasks']
+		if ($task) {
+			$map = @{deploy='deploy/deploy.build.ps1'; build='src/build.build.ps1'}
+			$file = $map[$task]
+			if ($file) {
+				(Invoke-Build ?? $file).get_Keys()
+			}
+		}
+	})]
     [string[]]$SubTasks,
 
     [string]$RootParam1
 
     # need to define the params for build and deploy to be able to pass them
-    # can we avoid to propagate these params from child scripts to the root script? 
+    # can we avoid to propagate these params from child scripts to the root script?
     # [string]$BuildParam1,
-	
+
     # [string]$BuildParam2
-    
+
     # [string]$DeployParam1,
-	
+
     # [string]$DeployParam2
 )
- 
+
 Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
 
@@ -35,11 +46,11 @@ if ([System.IO.Path]::GetFileName($MyInvocation.ScriptName) -ne 'Invoke-Build.ps
 	Invoke-Build -Task $Tasks -File $MyInvocation.MyCommand.Path @PSBoundParameters
 	return
 }
- 
+
 # delete Tasks params - SubTasks become Tasks for child scripts
 # clone the collection to ensure imutability
 $PSBoundParameters.Remove("Tasks") | out-null
- 
+
 task build {
     # just delegates to build.build.ps1
     Invoke-Build -Task $SubTasks -File "src/build.build.ps1" @PSBoundParameters
@@ -49,11 +60,11 @@ task deploy {
     # just delegates to deploy.build.ps1
     Invoke-Build -Task $SubTasks -File "deploy/deploy.build.ps1" @PSBoundParameters
 }
- 
+
 # Synopsis: Top level task.
 task roottask {
     # some of the top level tasks may compose Tasks from the child scripts
     Write-Output "root task $RootParam1"
 }
- 
+
 task . roottask
